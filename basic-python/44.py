d039f504cc1549f689e054509e2f927d16c9f17f
Phuong Nguyen <nhdphuong93@gmail.com>
2019-08-19 04:28:33 +0700

    lifo and parentheses validation

--- basic-python/44.py
+++ basic-python/44.py
@@ -3,3 +3,25 @@
 Write a Python class to find validity of a string of parentheses, '(', ')', '{', '}', '[' and ']. These brackets must be
 close in the correct order, for example "()" and "()[]{}" are valid but "[)", "({[)]" and "{{{" are invalid.
 """
+BRACKETS = {
+    "[" : "]",
+    "{" : "}",
+    "(" : ")"
+}
+
+
+def is_valid(string_to_check):
+    list_brackets = []
+    for bracket in string_to_check:
+        if bracket in BRACKETS:
+            list_brackets.append(BRACKETS[bracket])
+        elif not list_brackets or list_brackets.pop() != bracket:
+            return False
+    return not list_brackets
+
+
+print is_valid("}{")
+print is_valid("()")
+print is_valid("[)")
+print is_valid("({[)]")
+print is_valid("{{{")
--- basic-python/48.py
+++ basic-python/48.py
@@ -4,4 +4,12 @@ Write a Python program to create a LIFO queue.
 Expected Output:
 3 2 1 0
 """
+from urllib3.util import queue
 
+my_queue = queue.LifoQueue()
+
+for num in range(10):
+    my_queue.put(num)
+
+while not my_queue.empty():
+    print my_queue.get(),
