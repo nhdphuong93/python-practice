663a86e5e037ec2782d7afe886f9982302c9f5b1
Phuong Nguyen <nhdphuong93@gmail.com>
2019-08-19 17:36:18 +0700

    Cache decorator

--- basic-python/46.py
+++ basic-python/46.py
@@ -9,4 +9,34 @@ def fibonacci(n):
   else:
     return fibonacci(n-1) + fibonacci(n-2)
 """
+from functools import wraps
+
+
+def cache(func):
+    cache_dict = func.cache_dict = {}
+    @wraps(func)
+    def my_cache(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key in cache_dict:
+            print "Found cache"
+            return cache_dict[key]
+        else:
+            print "Add cache"
+            cache_dict[key] = func(*args, **kwargs)
+            return cache_dict[key]
+    return my_cache
+
+
+@cache
+def fibonacci(n):
+    assert n >= 0
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print fibonacci(5)
+print fibonacci(5)
+print fibonacci(3)
 
